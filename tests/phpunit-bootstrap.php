5ede1f7ceb3e1783413391b471b83f6c31aa3ec4
Patrick Müller <p.mueller@pcsg.de>
2026-08-06 09:44:27 +0000

    feat: erp demo data provider

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -9,3 +9,10 @@ if (!defined('QUIQQER_AJAX')) {
 }
 
 require_once __DIR__ . '/../../../../bootstrap.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/Contract/DemoDataCreatorInterface.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/Contract/DemoDataProviderInterface.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/DTO/CreatedDemoData.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/DTO/CreatedDemoDataCollection.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/DTO/DemoDataCreationContext.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/DTO/DemoDataReference.php';
+require_once __DIR__ . '/stubs/QUI/ERP/DemoData/DTO/DemoDataReferenceCollection.php';
---
+++ tests/stubs/QUI/ERP/DemoData/Contract/DemoDataProviderInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QUI\ERP\DemoData\Contract;
+
+use Doctrine\DBAL\Connection;
+use QUI\ERP\DemoData\Contract\DemoDataCreatorInterface;
+use QUI\Locale;
+
+if (!interface_exists(DemoDataProviderInterface::class, false)) {
+    interface DemoDataProviderInterface
+    {
+        public function getIdentifier(): string;
+
+        public function getTitle(?Locale $locale = null): string;
+
+        public function getDemoDataCreator(Connection $connection): DemoDataCreatorInterface;
+    }
+}
