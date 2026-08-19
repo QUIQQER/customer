471a37df7f1d5d6e0565febce75ff093b4838492
Henning <leutz@pcsg.de>
2026-08-19 18:41:43 +0200

    test: compare download entries by value

--- tests/integration/CustomerFilesTest.php
+++ tests/integration/CustomerFilesTest.php
@@ -275,8 +275,8 @@ final class CustomerFilesTest extends TestCase
             }
 
             $DownloadEntry = CustomerFiles::createDownloadEntry($this->customerUuid);
-            self::assertSame($DownloadEntry, CustomerFiles::getDownloadEntry($this->customerUuid));
-            self::assertSame($DownloadEntry, CustomerFiles::createDownloadEntry($this->customerUuid));
+            self::assertEquals($DownloadEntry, CustomerFiles::getDownloadEntry($this->customerUuid));
+            self::assertEquals($DownloadEntry, CustomerFiles::createDownloadEntry($this->customerUuid));
 
             $this->callable('downloadEntry_addFile')($basename, $this->customerUuid);
             self::assertTrue(CustomerFiles::isFileInDownloadEntry(
